89ce10712b0cd7e293fef412c05e16ae462afcc9
Jakub Sygnowski <sygnowski@gmail.com>
2014-08-23 13:47:06 +0200

    check if compiled

--- sample.cpp
+++ sample.cpp
@@ -14,19 +14,30 @@
 #include <set>
 using namespace std;
 
+#ifdef DEBUG
 inline void debug() {
+    cout << "\n";
 }
 template <typename _type, typename... type>
 	inline void debug(_type arg, const type&... next) {
 		cout << arg;
 		debug(next...);
 	}
+#else
+inline void debug(){
+}
+template <typename _type, typename... type>
+	inline void debug(_type arg, const type&... next) {
+	}
+#endif
 
-#define MP make_pair
+#define mp make_pair
 #define F first
 #define S second
+#define pb push_back
 
 typedef long long int64;
+typedef pair<int,int> pii;
 
 // ---------- TEMPLATE CODE ENDS HERE ----------
 
